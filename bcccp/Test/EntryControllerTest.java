e112e24d1bbef95456556743257cc7eb61d12b5c
nathasha fernando <nathasharu492@gmail.com>
2017-09-20 00:19:49 +1000

    Crete this package and test EntryController log message

---
+++ bcccp/Test/EntryControllerTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp.Test;
+
+import bcccp.carpark.entry.EntryController;
+
+/**
+ *
+ * @author nathasha
+ */
+public class EntryControllerTest {
+    
+    public static void main(String[] args){
+        
+        EntryController enCon = new EntryController();
+        enCon.log("Lassana malsha");
+        
+    }
+    
+}
