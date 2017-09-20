cfea139303941eb03aa4b27b4260abdfdd4eede9
mwarna03 <nathasharu492@gmail.com>
2017-09-21 07:37:05 +1000

    Test carpark class get name method

---
+++ bcccp/Test/CarparkTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp.Test;
+
+import bcccp.carpark.Carpark;
+
+/**
+ *
+ * @author nathasha
+ */
+public class CarparkTest {
+    
+    public static void main(String [] args){
+        
+        //10th commit check getname of the carpark class
+            Carpark cCon=new Carpark();
+            String c=cCon.getName();
+            System.out.println(c);
+    
+   
+    }
+   
+    
+}
