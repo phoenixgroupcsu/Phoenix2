705bedb8beb993d4085d4a210b3d9d4efdd93620
dwanna01 <30464706+dwanna01@users.noreply.github.com>
2017-09-23 10:33:32 +1000

    This is text class1
    This is text class1

---
+++ TestClass1.java
@@ -0,0 +1,38 @@
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.Font;
+import java.awt.Toolkit;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+ 
+
+@SuppressWarnings("serial")
+public class TestClass1 extends JFrame implements IPaystationUI {
+ 
+
+ private JPanel contentPane;
+ private JTextField displayTextField;
+ private JTextField barcodeTextField;
+ private IPaystationController controller;
+ private JTextArea ticketPrinterTextArea;
+
+
+
+public PaystationUI(int x, int y) {
+  setTitle("PayStation UI");
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  setBounds(x, y, 350, 710);
+  contentPane = new JPanel();
+  contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+  setContentPane(contentPane);
+  contentPane.setLayout(null);
+}
+ 
+
+}
