58791b2418fc59cad01174cc18c9d88c9fa7c362
dwanna01 <30464706+dwanna01@users.noreply.github.com>
2017-09-24 11:18:45 +1000

    Integration Test Class 2
     Integration Test Class 2

---
+++ IntegrationTestClass2.java
@@ -0,0 +1,44 @@
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.Font;
+import java.awt.Toolkit;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+ 
+
+@SuppressWarnings("serial")
+public class integrationTestClass2 extends JFrame implements IPaystationUI {
+ 
+
+ private JPanel contentPane;
+ private JTextField displayTextField;
+ private JTextField barcodeTextField;
+ private IPaystationController controller;
+ private JTextArea ticketPrinterTextArea;
+
+
+
+public PaystationUI(int x, int y) {
+		setTitle("PayStation UI");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(x, y, 350, 710);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "LCD Display", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(5, 5, 320, 106);
+		contentPane.add(panel);
+		panel.setLayout(null);
+}
+ 
+
+}
